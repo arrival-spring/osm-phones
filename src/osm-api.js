dd550897c66b85ccf0767fbe41380c8ee054503f
arrival-spring <77166354+arrival-spring@users.noreply.github.com>
2025-09-28 22:13:41 +0200

    Switch to standard node-fetch import

--- src/osm-api.js
+++ src/osm-api.js
@@ -1,4 +1,5 @@
 const { OVERPASS_API_URL } = require('./constants');
+const fetch = require('node-fetch');
 
 /**
  * Recursive unction to fetch admin_level=6 subdivisions from Overpass API.
@@ -9,7 +10,6 @@ const { OVERPASS_API_URL } = require('./constants');
  */
 async function fetchAdminLevels(divisionAreaId, divisionName, admin_level, retries = 3) {
     console.log(`Fetching all subdivisions for ${divisionName}...`);
-    const { default: fetch } = await import('node-fetch');
 
     const queryTimeout = 180;
 
@@ -64,7 +64,6 @@ async function fetchAdminLevels(divisionAreaId, divisionName, admin_level, retri
  */
 async function fetchOsmDataForDivision(division, retries = 3) {
     console.log(`Fetching data for division: ${division.name} (ID: ${division.id})...`);
-    const { default: fetch } = await import('node-fetch');
 
     const areaId = division.id + 3600000000;
     const queryTimeout = 600;
