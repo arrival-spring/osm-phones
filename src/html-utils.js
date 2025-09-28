a07e7d4f87be9bb72f0326fbcf0b424cd8637cf0
arrival-spring <77166354+arrival-spring@users.noreply.github.com>
2025-09-28 22:01:50 +0200

    Back live

--- src/html-utils.js
+++ src/html-utils.js
@@ -287,8 +287,7 @@ async function generateHtmlReport(countryName, subdivision, invalidNumbers, tota
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>${translate('countryReportTitle', locale, [countryName])}</title>
         ${favicon}
-        <!-- <link href="../styles.css" rel="stylesheet"> -->
-        <script src="https://cdn.tailwindcss.com"></script>
+        <link href="../styles.css" rel="stylesheet">
         <style>
             body { font-family: 'Inter', sans-serif; background-color: #f3f4f6; }
         </style>
@@ -546,8 +545,7 @@ function generateMainIndexHtml(countryStats, locale, translations) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>${translate('mainIndexTitle', locale)}</title>
         ${favicon}
-        <!-- <link href="./styles.css" rel="stylesheet"> -->
-        <script src="https://cdn.tailwindcss.com"></script>
+        <link href="./styles.css" rel="stylesheet">
         <style>
             body { font-family: 'Inter', sans-serif; background-color: #f3f4f6; }
         </style>
@@ -918,8 +916,7 @@ function generateCountryIndexHtml(countryName, groupedDivisionStats, totalInvali
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>${translate('countryReportTitle', locale, [countryName])}</title>
         ${favicon}
-        <!-- <link href="./styles.css" rel="stylesheet"> -->
-        <script src="https://cdn.tailwindcss.com"></script>
+        <link href="./styles.css" rel="stylesheet">
         <style>
             body { font-family: 'Inter', sans-serif; background-color: #f3f4f6; }
         </style>
